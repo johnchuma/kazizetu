fadd4a5802439847b401af5cc82380abe11cc72f
johnchuma <johnvchuma@gmail.com>
2024-01-28 23:10:26 +0300

    save changes

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -41,7 +41,7 @@ const Navbar = () => {
           setshowMenu(!showMenu)
         }} className="flex relative items-center space-x-3 border cursor-pointer  px-1 border-slate-400 py-[2px] md:py-0 rounded-full">
           <div className="aspect-squire bg-slate-950 rounded-full">
-            {userDetails != null ?<Image src={userDetails.image} height={30} width={30} className="rounded-full aspect-square object-cover"/>:
+            {userDetails != null ?<Image src={userDetails.image} height={30} width={30} className="rounded-full h-6 w-6 aspect-square object-cover"/>:
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3 h-3 text-white m-2 ">
               <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
             </svg>
